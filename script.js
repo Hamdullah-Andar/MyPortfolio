8bb89f06b948463480271453e8b7c401087baeb1
Hamdullah-Andar <hamdullahandar786@gmail.com>
2022-10-18 18:36:45 +0430

    Add script.js file

---
+++ script.js
@@ -0,0 +1,7 @@
+const humburgerBtn = document.getElementById('humburger-btn')
+    menubar = document.querySelector('ul')
+  menuExit = document.getElementById('menu-exit');
+
+  humburgerBtn.addEventListener('click', () => {
+    nav.classList.add('menu-bar');
+  })
